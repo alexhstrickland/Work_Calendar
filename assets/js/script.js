0bac96462fd28d728c4beeea1f36201fad80c935
alexhstrickland <alexhstrickland@gmail.com>
2021-01-07 22:57:03 -0500

    Started loop to change colors of time-blockd according to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var begDay = 9;
 var endDay = 18;
-currentTime = moment();
+currentTime = moment().format("hA");
 console.log(currentTime);
 
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
@@ -10,8 +10,10 @@ for (var i = begDay; i < endDay; i++) {
     var rowDiv = $("<div class='row'>");
 
     var hourDiv= $("<div class='hour col-md-1'>").text((moment().hour(i).format("hA")));
+    
+    
 
-    var timeBlock = $("<div class='time-block col-md-9'>");
+    var timeBlock = $("<div class='time-block col-md-9'>").attr("value", i);
     var description = $("<textarea></textarea>");
 
     var saveBtn = $("<div class='saveBtn col-md-1'>");
@@ -22,4 +24,9 @@ for (var i = begDay; i < endDay; i++) {
     $(".container").append(rowDiv.append(hourDiv, timeBlock.append(description), saveBtn.append(saveIcon)));
 };
 
-// if currentTime 
+
+
+
+
+//Stopped here
+if currentTime < 
