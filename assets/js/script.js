ae14604f52cfc73d2cb5aa59c6fdb6b0c5816459
alexhstrickland <alexhstrickland@gmail.com>
2021-01-07 21:24:12 -0500

    Added current day and date at top of the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var begDay = 9;
 var endDay = 18;
+currentTime = moment();
+console.log(currentTime);
+
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 for (var i = begDay; i < endDay; i++) {
     
@@ -7,7 +11,7 @@ for (var i = begDay; i < endDay; i++) {
 
     var hourDiv= $("<div class='hour col-md-1'>").text((moment().hour(i).format("hA")));
 
-    var timeBlock = $("<div class='time-block col-md-10'>");
+    var timeBlock = $("<div class='time-block col-md-9'>");
     var description = $("<textarea></textarea>");
 
     var saveBtn = $("<div class='saveBtn col-md-1'>");
@@ -17,3 +21,5 @@ for (var i = begDay; i < endDay; i++) {
 
     $(".container").append(rowDiv.append(hourDiv, timeBlock.append(description), saveBtn.append(saveIcon)));
 };
+
+// if currentTime 
